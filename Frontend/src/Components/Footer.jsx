1413385f4f56b699e94b24a2ba341277ef94bd40
Jatin <jatinhappy.singh@gmail.com>
2026-07-11 18:32:05 +0530

    Footer is complete

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,8 +1,64 @@
+import { Phone, Pin, Mail } from 'lucide-react'
+import {FaInstagram, FaFacebook , FaWhatsapp, FaXTwitter} from "react-icons/fa6"
 import React from 'react'
 
 function Footer() {
   return (
-    <div className='max-w-7xl bg-linear-to-r from-white via-[#f3e4ce] to-white border border-green-800 mb-3  mx-auto rounded-full h-17 bg-zinc-500 mt-5'>
+    <div className='max-w-7xl flex bg-linear-to-r items-center from-white via-[#f3e4ce] to-white border border-green-800 mb-3 px-25 justify-between mx-auto rounded-full h-50 bg-zinc-500 mt-5'>
+      <div className='flex flex-col gap-2'>
+        <h1 className='font-semibold bg-green-500/20 rounded-full px-2'>Quick Links</h1>
+        <ul className='ml-2'>
+          <li className='cursor-pointer hover:text-green-800'>Home</li>
+          <li className='cursor-pointer hover:text-green-800'>Medicine</li>
+          <li className='cursor-pointer hover:text-green-800'>Categories</li>
+          <li className='cursor-pointer hover:text-green-800'>About</li>
+        </ul>
+      </div>
+      <div className='flex  flex-col gap-2'>
+        <h1 className='font-semibold bg-green-500/20 rounded-full px-2 '>Customer Support</h1>
+        <ul className='ml-2'>
+          <li className='cursor-pointer hover:text-green-800'>FAQs</li>
+          <li className='cursor-pointer hover:text-green-800'>Privacy Policy</li>
+          <li className='cursor-pointer hover:text-green-800'>Terms & Conditions</li>
+          <li className='cursor-pointer hover:text-green-800'>Shipping Policy</li>
+        </ul>
+      </div>
+      <div className='flex flex-col gap-2.5 mt-1'>
+        <h1 className='font-semibold bg-green-500/20 rounded-full w-max px-2'>Contact</h1>
+        <ul className='flex flex-col gap-2'>
+          <div className='flex items-center'>
+            <Pin size={18} className='fill-green-800 mr-2 text-green-800'/>
+          <li>Satna, India</li>
+          </div>
+          <div className='flex items-center'>
+          <Phone size={18} className='fill-green-800 mr-1.5 text-green-800'/>
+          <li>+91 1234567890</li>
+          </div>
+          <div className='flex items-center'>
+            <Mail size={18} className='text-green-900 mr-2'/>
+          <li>Support@MediHive.com</li>
+          </div>
+        </ul>
+      </div>
+      <div className='flex flex-col gap-1 mt-5'>
+        <h1 className='font-semibold mb-1 bg-green-500/20 rounded-full px-2 w-max'>Follow Us</h1>
+        <div className='flex items-center cursor-pointer group'>
+          <FaInstagram size={15} className=' group-hover:text-pink-600 mr-2'/>
+        <ul className='mb-0.5'>Instagram</ul>
+        </div>
+        <div className=' flex group cursor-pointer items-center'>
+          <FaFacebook size={15} className='mr-2 group-hover:text-blue-700'/>
+        <ul>Facebook</ul>
+        </div>
+        <div className='flex group cursor-pointer items-center'>
+          <FaWhatsapp size={15} className='mr-2 group-hover:text-green-600'/>
+        <ul>Whatsapp</ul>
+        </div>
+        <div className='flex cursor-pointer items-center'>
+          <FaXTwitter size={15} className='mr-2 '/>
+        <ul className='font-semibold text-zinc-600'>Twitter</ul>
+        </div>
+      </div>
         
     </div>
   )
--- Frontend/src/Components/Testimonials.jsx
+++ Frontend/src/Components/Testimonials.jsx
@@ -4,10 +4,7 @@ import TestimonialCard from "./TestimonialCard";
 
 function Testimonials() {
   return (
-    <div className="max-w-7xl mx-auto mt-8 overflow-hidden">
-      <div className="absolute -top-32 -left-32 h-80 w-80 rounded-full bg-green-300/20 blur-3xl"></div>
-
-      <div className="absolute bottom-0 right-0 h-72 w-72 rounded-full bg-green-200/20 blur-3xl"></div>
+    <div className="max-w-7xl mx-auto mt-8 bg-white/10 overflow-hidden">
       <div className="relative z-10">
       <div className="ml-5 mt-4 mb-6">
         <h3 className="text-[13px] text-zinc-700 font-semibold">
